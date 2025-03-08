c59ac2cbb26c009b1d61eddd45bcb487b679632e
Yanshishuang <yash24oy@student.ju.se>
2025-03-08 19:15:20 +0100

    setup the animation of addtocart

---
+++ js/main.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const addToCartButton = document.getElementById("addtocart-button");
+  const productImg = document.querySelector(".productdetails-img");
+  const cartIcon = document.querySelector(".material-symbols-outlined");
+
+  addToCartButton.addEventListener("click", function () {
+    let flyingImg = productImg.cloneNode(true); //*Clone Image*/
+    flyingImg.style.position = "fixed";
+    flyingImg.style.width = "100px";
+    flyingImg.style.height = "100px";
+    flyingImg.style.zIndex = "1000"; /*Always before all elements*/
+
+    /*Calculate the initial position of the image*/
+    let imgRect = productImg.getBoundingClientRect();
+    let imgWidth = imgRect.width;
+    let imgHeight = imgRect.height;
+    flyingImg.style.left = imgRect.left + imgWidth / 2 - 50 + "px";
+    flyingImg.style.top = imgRect.top + imgHeight / 2 - 50 + "px";
+
+    /* add to the page */
+    document.body.appendChild(flyingImg);
+
+    /*Calculate target location (shopping cart)*/
+    let cartRect = cartIcon.getBoundingClientRect();
+    /* let the image fly to the cart */
+    flyingImg.style.transition = "all 1s ease-in-out";
+
+    /*Setting the delay*/
+    setTimeout(() => {
+      flyingImg.style.left = cartRect.left + "px";
+      flyingImg.style.top = cartRect.top + "px";
+      flyingImg.style.opacity = "0";
+    }, 100);
+    /* remover the image from the page after the animation*/
+    setTimeout(() =>{
+        document.body.removeChild(flyingImg);
+    },1100);
+  });
+});
